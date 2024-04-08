d1f3bda46e0fe65255c5e80e4b99fa6f44f85f99
SeoJun <101031710+Polaroidd@users.noreply.github.com>
2024-04-08 17:25:44 +0900

    Title: 코드트리 빵, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240408/코드트리 빵/codetree-mon-bread.java
+++ 240408/코드트리 빵/codetree-mon-bread.java
@@ -12,10 +12,9 @@ class Node implements Comparable<Node>{
 
     @Override
     public int compareTo(Node other){
-//        if(this.t!=other.t){
-//            return this.t-other.t;
-//        }else 
-        if(this.r!=other.r){
+        if(this.t!=other.t){
+            return this.t-other.t;
+        }else if(this.r!=other.r){
             return this.r-other.r;
 
         }else{
@@ -110,19 +109,19 @@ public class Main {
         }
     }
     static boolean arrived(int time){
-//         System.out.println("arrived : "+time);
+        //  System.out.println("arrived : "+time);
         
         int res = 0;
         for(int i=0;i<M;i++){
             if(arrived[i]){
-//                 System.out.print(i);
+                //  System.out.print(i);
                 wallmap[storelst[i].r][storelst[i].c] = true;
             }else{
                 res++;
             }
         }
-//         System.out.println();
-//         System.out.println(res);
+        //  System.out.println();
+        //  System.out.println(res);
         
         if(res == 0){
             return true;
@@ -144,10 +143,8 @@ public class Main {
     }
     static Node bfs(Node nde, int idx){
         boolean[][] visited = new boolean[N][N];
-        Node minnode = new Node(Integer.MAX_VALUE,Integer.MAX_VALUE,0);
-        int time = Integer.MAX_VALUE;
-        boolean flag = false;
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+
         Queue<Node> que = new LinkedList<>();
         que.add(nde);
         while(!que.isEmpty()){
@@ -164,31 +161,20 @@ public class Main {
             for(int[] temp:dir){
                 int r = temp[0]+n.r;
                 int c = temp[1]+n.c;
-                if(r<0||r>=N||c<0||c>=N||visited[r][c]){
+                if(r<0||r>=N||c<0||c>=N||visited[r][c]||wallmap[r][c]){
                     continue;
                 }
                 if(basemap[r][c]){
-					if(!flag) {
-						time = n.t;
-					}
-                    flag = true;
-                    pq.add(new Node(r,c,n.t));
+                    basemap[r][c] = false;
+                    wallmap[r][c] = true;
+                    return new Node(r,c,idx);
                 }
-                if(wallmap[r][c]){
-                    continue;
-                }
-                if(!flag) {
-                    que.add(new Node(r,c,n.t+1));
-                	
-                }
+                
+                que.add(new Node(r,c,0));
 
             }
-            
         }
-        while(pq.peek().t>time) {
-        	pq.poll();
-        }
-        return pq.peek();
+        return new Node(-1,-1,-1);
 
     }
     static Node bfs2(Node nde, int idx){
@@ -219,8 +205,6 @@ public class Main {
                 	while(now_node.parnode!=nde) {
                 		now_node = now_node.parnode;
                 	}
-//                	System.out.println(nde.r + " "+nde.c+ " "+now_node.r+" "+now_node.c);
-                    
                 	return new Node(now_node.r,now_node.c,0);
                 }
                 que.add(now_node);
